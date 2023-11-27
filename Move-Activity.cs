57873c0c4ea1b8614d41d11c1c6f7eadaab8c9af
pluka073 <71723381+pluka073@users.noreply.github.com>
2023-11-26 22:08:47 -0700

    Final push
    Adding instruction and activity script

---
+++ Move-Activity.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObject : MonoBehaviour
+{
+    // TODO Create Rigidbody instance
+    // TODO Time interval to move the object
+    // TODO Total time to move the object
+    // TODO Timer to keep track of elapsed time
+    // TODO Distance to move in one block
+
+    void Start()
+    {
+        // TODO Get the Rigidbody component attached to the GameObject
+
+    }
+
+    void Update()
+    {
+        // TODO Increment the timer each frame
+
+
+        // TODO Check if the total time has not elapsed
+
+            // TODO Check if it's time to move the object
+
+                // TODO Calculate the force needed to move the object forward
+
+
+                // TODO Apply the force to the Rigidbody
+
+    }
